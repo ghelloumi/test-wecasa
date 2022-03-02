d55bce9b139b17a6ed5809eaf1331825274fd394
Ghassen ELLOUMI <ghassen.elloumi@vo2-group.com>
2022-03-03 00:28:03 +0100

    create Util : convert duration to hours

--- src/Utils.js
+++ src/Utils.js
@@ -1,1 +1,8 @@
 export const convertToEuro = (value) => `${(value / 100).toFixed(2).toLocaleString()} €`;
+
+export const displayDurationInHours = (duration) => {
+  const hours = Math.trunc(duration / 60);
+  const minutes = duration % 60;
+
+  return `${hours}h${minutes}`;
+};
