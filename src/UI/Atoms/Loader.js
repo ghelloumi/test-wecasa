7e95451c96cea9f1e0deee142cf45e3f5ecbfba5
Ghassen ELLOUMI <ghassen.elloumi@vo2-group.com>
2022-03-03 10:20:34 +0100

    Create Loader component

---
+++ src/UI/Atoms/Loader.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  border: 10px solid #f3f3f3;
+  border-top: 10px solid #3498db;
+  border-radius: 50%;
+  width: 80px;
+  height: 80px;
+  animation: spin 1s linear infinite;
+  margin: ${({ theme }) => theme.sizes.xl}px;
+`;
+
+const Loader = () => <Container />;
+
+export default Loader;
