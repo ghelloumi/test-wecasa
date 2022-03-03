79cdb9ec640144a6b34e1d09a24cfb1f07d984ac
Ghassen ELLOUMI <ghassen.elloumi@vo2-group.com>
2022-03-03 01:26:40 +0100

    Create useClickOutside custom hook

---
+++ src/hooks/useClickOutside.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+
+export const useClickOutside = (ref) => {
+  const [clickedOutside, setClickedOutside] = useState(false);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setClickedOutside(true);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [ref]);
+
+  return clickedOutside;
+};
